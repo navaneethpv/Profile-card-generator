0e41f11b94968a3781c261266963023a66ff1a2e
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-10 01:31:11 +0300

    Centers card on screen
    Centers the card component on the screen by adding flexbox styling to the parent div in the App component.
    Removes unnecessary height and width styling from the card component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Cards from "./components/Cards";
 
 function App() {
   return (
-    <>
+    <div className="flex justify-center bg-gray-200 w-screen h-screen items-center gap-13">
       <Cards />
-    </>
+    </div>
   );
 }
 
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,31 +1,37 @@
-import React from 'react'
-import { CiMail} from "react-icons/ci";
-import { FaTwitter} from "react-icons/fa";
-
+import React from "react";
+import { CiMail } from "react-icons/ci";
+import { FaTwitter } from "react-icons/fa";
 
 const Cards = () => {
   return (
-    <div className='h-screen w-screen bg-gray-200 flex items-center justify-center'>
-        <div className='w-[400px] h-[470px] bg-white rounded-lg shadow-2xl'>
-            <img className='size-[120px] rounded-full mx-auto mt-[60px] object-cover object-center border-2 border-cyan-400' src="https://images.pexels.com/photos/1784289/pexels-photo-1784289.jpeg?cs=srgb&dl=adorable-animal-baby-1784289.jpg&fm=jpg" alt="profile" />  
-            <p className='text-center mt-5 font-bold text-[21px]'>John Doe</p>
-            <p className='text-center text-gray-400 mt-5 font-semibold text-[16px]'> WordPress developer</p>
-            <div className='bg-blue-100 flex justify-around w-1/2 mx-auto px-[13px] py-1 mt-4 rounded-lg'>
-                <p className='text-blue-400 font-semibold text-[14px]'>Founder & Editor</p>
-            </div>
-            <div className='grid grid-cols-2 mt-7 gap-1 max-w-[300px] mx-auto'>
-                <div className='flex bg-gray-100 w-[130px] mx-auto justify-center rounded-md p-3 gap-3 hover:cursor-pointer hover:bg-gray-200 transition-all duration-150 shadow-md'>
-                    <CiMail className='my-auto'/>
-                    <p>Email</p>
-                </div>
-                <div className='flex bg-gray-100 w-[130px] mx-auto justify-center rounded-md p-3 gap-3 hover:cursor-pointer hover:bg-gray-200 transition-all duration-150 shadow-md'>
-                    <FaTwitter className='my-auto'/>
-                    <p>Twitter</p>
-                </div>
-            </div>
+    <div className="w-[400px] h-[470px] bg-white rounded-lg shadow-2xl">
+      <img
+        className="size-[120px] rounded-full mx-auto mt-[60px] object-cover object-center border-2 border-cyan-400"
+        src="https://images.pexels.com/photos/1784289/pexels-photo-1784289.jpeg?cs=srgb&dl=adorable-animal-baby-1784289.jpg&fm=jpg"
+        alt="profile"
+      />
+      <p className="text-center mt-5 font-bold text-[21px]">John Doe</p>
+      <p className="text-center text-gray-400 mt-5 font-semibold text-[16px]">
+        {" "}
+        WordPress developer
+      </p>
+      <div className="bg-blue-100 flex justify-around w-1/2 mx-auto px-[13px] py-1 mt-4 rounded-lg">
+        <p className="text-blue-400 font-semibold text-[14px]">
+          Founder & Editor
+        </p>
+      </div>
+      <div className="grid grid-cols-2 mt-7 gap-1 max-w-[300px] mx-auto">
+        <div className="flex bg-gray-100 w-[130px] mx-auto justify-center rounded-md p-3 gap-3 hover:cursor-pointer hover:bg-gray-200 transition-all duration-150 shadow-md">
+          <CiMail className="my-auto" />
+          <p>Email</p>
         </div>
+        <div className="flex bg-gray-100 w-[130px] mx-auto justify-center rounded-md p-3 gap-3 hover:cursor-pointer hover:bg-gray-200 transition-all duration-150 shadow-md">
+          <FaTwitter className="my-auto" />
+          <p>Twitter</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cards;
