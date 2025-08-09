dc8cacd7cd35bd70cb4d31559cc084949b6f4727
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-10 02:27:17 +0300

    Changed card position

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Form from "./components/Forms";
 function App() {
   return (
     <div className="flex justify-center bg-gray-200 w-screen h-screen items-center gap-13">
-      <Cards />
       <Form />
+      <Cards />
     </div>
   );
 }
