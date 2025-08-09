1ed50e017ea04aa0bee10ea5c1212aa71f701040
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-10 02:26:22 +0300

    Adds form component
    Adds the form component to the application layout,
    positioning it alongside the existing cards component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Cards from "./components/Cards";
+import Form from "./components/Forms";
 
 function App() {
   return (
     <div className="flex justify-center bg-gray-200 w-screen h-screen items-center gap-13">
       <Cards />
+      <Form />
     </div>
   );
 }
