8de7845fa905415d3fb44557683a0f9a0b0c565c
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-14 09:02:41 +0300

    Refactor App component layout for improved structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import Cards from "./components/Cards";
 import Form from "./components/Forms";
-import ProfileImage from "./assets/Image.jpeg"
+import ProfileImage from "./assets/Image.jpeg";
 
 function App() {
   const [Profile, SetProfile] = useState({
-    name: 'John Doe',
-    role: 'WordPress Developer',
-    title: 'Founder & Editor',
-    image: ProfileImage
+    name: "John Doe",
+    role: "WordPress Developer",
+    title: "Founder & Editor",
+    image: ProfileImage,
   });
-  
-  const UpdateData = (newData) =>{
-    SetProfile(newData);
-  }
 
+  const UpdateData = (newData) => {
+    SetProfile(newData);
+  };
 
   return (
     <div className="flex justify-center bg-gray-200 w-screen h-screen items-center gap-13">
-      <Form Profile={Profile} UpdateData={UpdateData} />
-      <Cards Profile = {Profile}/>
+      <div className="flex  gap-10 bg-gray-300 rounded-lg p-10 shadow-2xl">
+        <Form Profile={Profile} UpdateData={UpdateData} />
+        <Cards Profile={Profile} />
+      </div>
     </div>
   );
 }
